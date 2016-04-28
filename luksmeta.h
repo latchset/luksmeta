2f6ebd67fc4f335686cb462f915f664bf8fe7651
Nathaniel McCallum <npmccallum@redhat.com>
2016-04-28 15:40:04 -0400

    Add API documentation comments

--- luksmeta.h
+++ luksmeta.h
@@ -23,16 +23,84 @@
 
 typedef uint8_t luksmeta_uuid_t[16];
 
+/**
+ * Initializes metadata storage on a LUKSv1 device
+ *
+ * @param cd crypt device handle
+ * @return Zero on success or negative errno value otherwise.
+ *
+ * @note This function returns -EALREADY if a valid header already exists.
+ * @note This function returns -ENOSPC if there is insufficient space.
+ */
+#if defined(__GNUC__)
+int __attribute__((nonnull(1), warn_unused_result))
+#else
 int
+#endif
 luksmeta_init(struct crypt_device *cd);
 
+/**
+ * Gets metadata from the specified slot
+ *
+ * If buf is NULL or size is smaller than the metadata, this function returns
+ * the size of the buffer needed and the uuid.
+ *
+ * @param cd crypt device handle
+ * @param slot requested metadata slot
+ * @param uuid the UUID of the metadata (output)
+ * @param buf output buffer for metadata (output)
+ * @param size size of buf
+ * @return The number of bytes in the metadata or negative errno value.
+ *
+ * @note This function returns -ENOENT if the device has no luksmeta header.
+ * @note This function returns -EINVAL if the header or slot data is corrupted.
+ * @note This function returns -EBADSLT if the specified slot is empty.
+ */
+#if defined(__GNUC__)
+int __attribute__((nonnull(1, 3), warn_unused_result))
+#else
 int
+#endif
 luksmeta_get(struct crypt_device *cd, int slot,
              luksmeta_uuid_t uuid, uint8_t *buf, size_t size);
 
+/**
+ * Sets metadata to the specified slot
+ *
+ * @param cd crypt device handle
+ * @param slot requested metadata slot
+ * @param uuid UUID of the metadata
+ * @param buf input buffer for metadata
+ * @param size size of buf
+ * @return The number of bytes in the metadata or negative errno value.
+ *
+ * @note This function returns -ENOENT if the device has no luksmeta header.
+ * @note This function returns -EINVAL if the header is corrupted.
+ * @note This function returns -EBADSLT if the specified slot is not empty.
+ * @note This function returns -ENOSPC if there is insufficient space.
+ */
+#if defined(__GNUC__)
+int __attribute__((nonnull(1, 3, 4), warn_unused_result))
+#else
 int
+#endif
 luksmeta_set(struct crypt_device *cd, int slot,
              const luksmeta_uuid_t uuid, const uint8_t *buf, size_t size);
 
+/**
+ * Deletes metadata from the specified slot
+ *
+ * @param cd crypt device handle
+ * @param slot requested metadata slot
+ * @return Zero on success or negative errno value otherwise.
+ *
+ * @note This function returns -ENOENT if the device has no luksmeta header.
+ * @note This function returns -EINVAL if the header is corrupted.
+ * @note This function returns -EBADSLT if the specified slot is empty.
+ */
+#if defined(__GNUC__)
+int __attribute__((nonnull(1)))
+#else
 int
+#endif
 luksmeta_del(struct crypt_device *cd, int slot);
